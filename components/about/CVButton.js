fe40bda13a148417b9fa79a02de753f4bb3ba3fb
harelpanker <harelpanker@gmail.com>
2022-09-18 07:51:56 +0200

    new cv

--- components/about/CVButton.js
+++ components/about/CVButton.js
@@ -5,7 +5,7 @@ const CVButton = () => {
   return (
     <a
       className='fixed z-50 whitespace-pre top-auto left-auto bottom-5 right-5 xl:right-[5vw] font-medium flex gap-2 items-center rounded-full px-4 py-1 bg-purple-300 text-purple-700 transition hover:bg-purple-700 hover:text-slate-50'
-      href='https://drive.google.com/file/d/1ONvl9sFpVvL5qA5UTVXBHMUal88V8_8O/view?usp=sharing'
+      href='https://drive.google.com/file/d/1p7XXNsJm22Cts7lMUIx7AvGNBP0nIM6Y/view?usp=sharing'
       target='_blank'
       download='Harel Panker CV - 2021'
       rel='noopener noreferrer'>
