befff12b37772ada141bed47f1fcc2a723b9f8bd
harelpanker <harelpanker@gmail.com>
2022-09-27 08:29:25 +0200

    google analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,28 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import Layout from '../components/share/Layout';
+import Script from 'next/script';
 // import '@splidejs/react-splide/css';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy='lazyOnload'>
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+
       <Layout>
         <Component {...pageProps} />
       </Layout>
