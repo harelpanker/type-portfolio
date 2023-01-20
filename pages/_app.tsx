481bb0565ce34db268a2f14b8d742903fa47d854
harelpanker <harelpanker@gmail.com>
2023-01-20 07:55:19 +0100

    adding GTM

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,21 +15,20 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       {/* Google analytics - start */}
-      <Script
+      {/* <Script
         id='gtm'
         strategy='lazyOnload'
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+      /> */}
 
-      <Script strategy='lazyOnload' id='analytics'>
+      <Script id='google-tag-manager' strategy='afterInteractive'>
         {`
-        window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
-        gtag('js', new Date());
-        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-        page_path: window.location.pathname,
-        });
-    `}
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-W8D8DVL');
+        `}
       </Script>
       {/* Google analytics - end */}
       <div className={`${jakarta.variable} font-sans`}>
